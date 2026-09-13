698b7af5a94f87e7a026b521a5c835c86c247422
mcr927 <46036987+mcr927@users.noreply.github.com>
2026-09-14 04:02:34 +0900

    chore: DesignSystemにリソースバンドルを追加

--- LocalPackages/GalleryPackage/Package.swift
+++ LocalPackages/GalleryPackage/Package.swift
@@ -51,6 +51,7 @@ let package = Package(
         // Modelsに依存させない。作品セルのようなドメイン依存のViewは画面モジュール側に置く。
         .target(
             name: "DesignSystem",
+            resources: [.process("Resources")],
             swiftSettings: uiSwiftSettings
         ),
 
